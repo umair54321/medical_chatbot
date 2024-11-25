ea52150c7ca1c3168405751c3eedd5125eab19e3
umair54321 <uk390507@gmail.com>
2024-11-26 02:37:00 +0500

    add all files

---
+++ app.py
@@ -0,0 +1,14 @@
+from src.mlproject.logger import logging
+from src.mlproject.exception import CustomException
+from src.mlproject.components.data_ingestion import DataIngestion
+import sys
+
+if __name__=="__main__":
+    logging.info("The execution has started")
+    
+    try:
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
